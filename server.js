f3617e7b0e70c68ce8d8406ecb1447ac0b570019
btrz <btrz.developer@gmail.com>
2020-05-16 13:32:57 +0200

    Basic body and query trace

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 const router = express.Router();
 
+const app = express();
+app.use(bodyParser.json()); // Cant send full parser, but specific method. Sets content-types, it can be .urlenconded(extended:false) to send it as form
 app.use(router);
 
-router.get('/', function( req, res ) {
-    res.send('Eo!')
+router.get('/message', function( req, res ) {
+    res.send('Messages list');
+});
+
+router.delete('/message', function( req, res ) {
+    //console.info(req.query);
+    console.info(req.body);
+    res.send('Message "'+req.body.text+'" deleted');
 });
 
 app.listen(3000);
-console.info('App is listening at port 3000');
+console.info('App is listening at port 3000'); 
