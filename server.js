21fb2b707121f70818468495baa055a6f29e0d36
btrz <btrz.developer@gmail.com>
2020-05-16 13:15:21 +0200

    Basic express routes config

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
-
 const app = express();
+const router = express.Router();
 
-app.use('/', function( req, res ) {
+app.use(router);
+
+router.get('/', function( req, res ) {
     res.send('Eo!')
 });
-    
+
 app.listen(3000);
 console.info('App is listening at port 3000');
