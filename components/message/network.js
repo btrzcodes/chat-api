46449f58e693fc3d7d1c52350da761ab4a19bf6d
btrz <btrz.developer@gmail.com>
2020-05-16 20:29:26 +0200

    Message controller created + responses & network improovement

--- components/message/network.js
+++ components/message/network.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+const controller = require('./controller')
 const response = require('../../network/response');
 
+router.post('/', function( req, res ) {
+    let body = req.body;
+
+    controller.addMessage(body.user, body.message)
+        .then( (fullMessage) => {
+            response.success(req, res, 'Message correclty created', 201, body);
+        })
+        .catch((err) => {
+            response.error(req, res, 'Message could not be created', 400, err);
+        });
+});
 
 router.get('/', function( req, res ) {
     res.header({
         "custom-header": "This is my custom header!"
     })
     response.success(req, res, 'Messages list');
-});
-
-router.post('/', function( req, res ) {
-    if( !req.query.error ) {
-        response.success(req, res, 'Message correclty created', 201);
-    } else {
-        response.error(req, res, 'Message could not be created', 500, 'Internal error reason');
-    }
 });
 
 router.delete('/', function( req, res ) {
--- network/response.js
+++ network/response.js
@@ -1,15 +1,13 @@
-exports.success = function (req, res, message, status = 200) {
+exports.success = function (req, res, info, status = 200, body) {
     res.status(status)
         .send({
-            error:'',
-            body : {
-                message
-            }
+            info,
+            body
         });
 }
 
 exports.error = function (req, res, error, status = 500, details) { // TODO FIX: if status is emty and details added it crashes the app. Fix this!
-    console.error('[Error while req to '+req.originalUrl+']: ', details); // for security reasons, internally logs the error but does not serve it as a response
+    console.error('[Error while '+req.method+' @ '+req.originalUrl+']: ', details); // for security reasons, internally logs the error but does not serve it as a response
     res.status(status)
         .send({
             error
